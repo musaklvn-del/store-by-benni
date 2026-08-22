e9c9d0b645555d807e16c07088c55ef07b4a58d3
musaklvn-del <musaklvn@gmail.com>
2026-08-22 20:32:13 +0100

    Add original product image variant loader

---
+++ exact-originals.js
@@ -0,0 +1,4 @@
+(()=>{
+window.BENNI_VARIANTS=window.BENNI_VARIANTS||{};
+window.BENNI_VARIANTS['simmi']=[{src:'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD...',label:'Burgundy',filter:'none'}];
+})();
