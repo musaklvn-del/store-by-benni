4aef09e340659b12b16054e723df75a60b91e09c
musaklvn-del <musaklvn@gmail.com>
2026-08-22 20:34:30 +0100

    Remove unused placeholder photo loader

--- exact-originals.js
+++
@@ -1,4 +0,0 @@
-(()=>{
-window.BENNI_VARIANTS=window.BENNI_VARIANTS||{};
-window.BENNI_VARIANTS['simmi']=[{src:'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD...',label:'Burgundy',filter:'none'}];
-})();
